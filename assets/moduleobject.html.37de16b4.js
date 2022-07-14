64b4676b7a7b02fc82bf926adbafaebce1598301
undefined <960504980@qq.com>
2022-07-14 09:52:40 +0800

    文档修改

---
+++ assets/moduleobject.html.37de16b4.js
@@ -0,0 +1,1 @@
+const e={key:"v-665a60c8",path:"/zh/moduledevelop/moduleobject.html",title:"\u7EC4\u4EF6\u5BF9\u8C61",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1657517932e3,contributors:[{name:"undefined",email:"960504980@qq.com",commits:1},{name:"yunit",email:"960504980@qq.com",commits:1}]},filePathRelative:"zh/moduledevelop/moduleobject.md"};export{e as data};
